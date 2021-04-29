d25d500b1e9ebcabc5050cb170a7f275ff15fd9a
ncc220 <nccdn6@mail.missouri.edu>
2021-04-28 20:55:28 -0500

    more player movement

--- Assets/Scenes/CameraController.cs
+++ Assets/Scenes/CameraController.cs
@@ -4,21 +4,36 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    //code from https://www.youtube.com/watch?v=nfX8cnRxrHg
     public Transform playerBody;
-    public float mouseSensitivity = 300f;
+    public float mouseSensitivity = 1f;
+
+    private float x = 0;
+    private float y = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        x += -Input.GetAxis("Mouse Y") * mouseSensitivity;
+        y += Input.GetAxis("Mouse X") * mouseSensitivity;
 
-        playerBody.Rotate(Vector3.up * mouseX);
+        x = Mathf.Clamp(x, -90, 90);
+
+        transform.localRotation = Quaternion.Euler(x, 0, 0);
+        playerBody.transform.localRotation = Quaternion.Euler(0, y, 0);
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(Cursor.lockState == CursorLockMode.Locked)
+            {
+                Cursor.lockState = CursorLockMode.None;
+            }
+        }
     }
 }
--- Assets/Scenes/PlayerMovement.cs
+++ Assets/Scenes/PlayerMovement.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    private Rigidbody _body;
-    public float speed = 10;
-    public float acceleration = 25;
-    public float jumpVelocity = 4;
+    // private Rigidbody _body;
+    // public float speed = 10;
+    // public float acceleration = 25;
+    // public float jumpVelocity = 4;
+
+    //code from https://www.youtube.com/watch?v=nfX8cnRxrHg
+    public Rigidbody _body;
+
+    public float moveSpeed = 6f;
+    public float jumpForce = 1f;
+
+    public LayerMask layerMask;
+
+    bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
     {
+        // _body = GetComponent<Rigidbody>();
         _body = GetComponent<Rigidbody>();
     }
 
@@ -20,42 +31,60 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         
-        if(Input.GetKeyDown(KeyCode.Space))
+        // if(Input.GetKeyDown(KeyCode.Space))
+        // {
+        //     var velocity = _body.velocity;
+        //     velocity.y = jumpVelocity;
+        //     _body.velocity = velocity;
+        // }
+
+        isGrounded = Physics.CheckSphere(new Vector3(transform.position.x, transform.position.y - 1, transform.position.z), 0.4f, layerMask);
+
+
+        float x = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        float y = Input.GetAxisRaw("Vertical") * moveSpeed;
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            var velocity = _body.velocity;
-            velocity.y = jumpVelocity;
-            _body.velocity = velocity;
+            _body.velocity = new Vector3(_body.velocity.x, jumpForce, _body.velocity.z);
         }
+
+        Vector3 move = transform.right * x + transform.forward * y;
+
+
+        _body.velocity = new Vector3(move.x, _body.velocity.y,move.z);
+
     }
 
-    private void FixedUpdate()
-    {
-        var targetVelocity = Vector3.zero;
-        var direction = _body.rotation;
-        //targetVelocity.w = direction.w;
+    // private void FixedUpdate()
+    // {
+    //     var targetVelocity = Vector3.zero;
+    //     var direction = _body.rotation;
+    //     //targetVelocity.w = direction.w;
 
-        if(Input.GetKey(KeyCode.W))
-        {
-            targetVelocity.z += 1;
-        }
-        if(Input.GetKey(KeyCode.S))
-        {
-            targetVelocity.z -= 1;
-        }
-        if(Input.GetKey(KeyCode.D))
-        {
-            targetVelocity.x += 1;
-        }
-        if(Input.GetKey(KeyCode.A))
-        {
-            targetVelocity.x -= 1;
-        }
+    //     if(Input.GetKey(KeyCode.W))
+    //     {
+    //         targetVelocity.z += 1;
+    //     }
+    //     if(Input.GetKey(KeyCode.S))
+    //     {
+    //         targetVelocity.z -= 1;
+    //     }
+    //     if(Input.GetKey(KeyCode.D))
+    //     {
+    //         targetVelocity.x += 1;
+    //     }
+    //     if(Input.GetKey(KeyCode.A))
+    //     {
+    //         targetVelocity.x -= 1;
+    //     }
 
-        targetVelocity = targetVelocity.normalized;
-        targetVelocity *= speed;
+    //     targetVelocity = targetVelocity.normalized;
+    //     targetVelocity *= speed;
 
-        targetVelocity = new Vector3(targetVelocity.x, _body.velocity.y, targetVelocity.z);
+    //     targetVelocity = new Vector3(targetVelocity.x, _body.velocity.y, targetVelocity.z);
 
-        _body.velocity = Vector3.MoveTowards(_body.velocity, targetVelocity, acceleration * Time.deltaTime);
-    }
+    //     _body.velocity = Vector3.MoveTowards(_body.velocity, targetVelocity, acceleration * Time.deltaTime);
+    // }
 }
